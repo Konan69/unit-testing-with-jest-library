0336fde0cfc72a239784b02d9fcf27e031c29cb8
Konan <kixeyems0@gmail.com>
2023-09-05 20:34:16 +0100

    refactored code

--- lib.js
+++ lib.js
@@ -3,9 +3,7 @@ const db = require('./db')
 
 // Testing numbers 
 module.exports.absolute = function(number){
-  if (number > 0) return number
-  if (number < 0) return -number
-  return 0
+  return (number >= 0)? number: -number
 }
 
 // Testing strings
